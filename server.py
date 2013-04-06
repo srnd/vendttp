b785725aeef0c07f88eb9f133bb735464fed604e
Blaise Ritchie <Blaise@heuxbis.(none)>
2013-04-06 16:38:40 -0700

    made no RFID scanner branch

--- server.py
+++ server.py
@@ -32,39 +32,40 @@ def Recv(s, t):
       RUNNING = False
       return
 
-def Com(ser, s):
-  while True:
-    ser.setDTR(False)
-    ser.flushInput()
-    ser.setDTR(True)
-    str = ""
-    i = 'G'
-    while i != '\r':
-      i = ser.read()
-      if i != '\n' and i != '\r':
-        str = str + i
-    ser.setDTR(False)
-    ser.flushInput()
-    s.send("<response type=\"account\"><account id=\"" + str + "\" name=\"" + json.loads(urllib.urlopen("http://my.studentrnd.org/api/user/rfid?rfid=" + str).read())['username'] + "\" /></response>")
-    time.sleep(2)
+#def Com(ser, s):
+#  while True:
+#    ser.setDTR(False)
+#    ser.flushInput()
+#    ser.setDTR(True)
+#    str = ""
+#    i = 'G'
+#    while i != '\r':
+#      i = ser.read()
+#      if i != '\n' and i != '\r':
+#        str = str + i
+#    ser.setDTR(False)
+#    ser.flushInput()
+#    s.send("<response type=\"account\"><account id=\"" + str + "\" name=\"" + 
+#json.loads(urllib.urlopen("http://my.studentrnd.org/api/user/rfid?rfid=" + str).read())['username'] + "\" /></response>")
+#    time.sleep(2)
 
 
 s = socket.socket()
 s.bind((HOST,PORT))
 s.listen(5)
 
-ser = None
-for i in range(1, 10):
-  try:
-    ser = serial.Serial(port = '/dev/com' + str(i), baudrate = 2400, parity = serial.PARITY_NONE, stopbits = 1,
-bytesize = 8)
-    break
-  except:
-    continue
+#ser = None
+#for i in range(1, 10):
+#  try:
+#    ser = serial.Serial(port = '/dev/com' + str(i), baudrate = 2400, parity = serial.PARITY_NONE, stopbits = 1,
+#bytesize = 8)
+#    break
+#  except:
+#    continue
 
-if ser == None:
-  print "No RFID scanner detected. Exitting..."
-  exit()
+#if ser == None:
+#  print "No RFID scanner detected. Exitting..."
+#  exit()
 
 while True:
   cs, address = s.accept()
@@ -75,7 +76,7 @@ while True:
 
   t = threading.Thread(target = Send, args=(cs,)).start()
   threading.Thread(target = Recv, args=(cs,t)).start()
-  threading.Thread(target = Com, args=(ser,cs)).start()
+#  threading.Thread(target = Com, args=(ser,cs)).start()
 
 #while True:
 #  cs.send(raw_input() + "\r\n")
