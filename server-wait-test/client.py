5667aaa641ef736a69eec149f00b8c74a055d102
George Matter VI <George.Matter.VI@gmail.com>
2013-04-09 22:32:41 -0700

    Test of a new program structure
    Testing a new server/client program structure (\server-wait-test\) to
    allow the server or clients to be disconnected or restarted without the
    whole system crashing.

---
+++ server-wait-test/client.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import time, socket, threading
+
+HOST="localhost"
+PORT=8636
+
+sock = None
+
+def send():
+  global sock
+  while True:
+    message = raw_input()
+    try:
+      sock.send(message)
+    except:
+      print "[ERROR] not connected to a server"
+
+def receive():
+  global sock
+  while True:
+    try:
+      sock = socket.socket()
+      sock.connect((HOST,PORT))
+      print "Successfully connected to server"
+    except:
+      time.sleep(2)
+      continue
+    while True:
+      try:
+        message = sock.recv(500).rstrip()
+        if len(message) != 0:
+          print "from server: " + message
+        else:
+          break
+      except:
+        break
+    print "Disconnected from server"
+    sock = None
+
+print "Starting test client #2. Attempting to connect to server"
+
+threading.Thread(target=send).start()
+threading.Thread(target=receive).start()
