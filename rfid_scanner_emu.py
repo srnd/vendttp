153e5171da956c8f081d93cc93774966ff273f6f
George Matter VI <George.Matter.VI@gmail.com>
2013-04-24 22:21:56 -0700

    made rfid_scanner_emu.py more stable

--- rfid_scanner_emu.py
+++ rfid_scanner_emu.py
@@ -1,33 +1,47 @@
 #!/usr/bin/env python
 
-import time, socket
+import time, socket, threading
 
 HOST="localhost"
 PORT=8638
 
 sock = None
 
-print "RFID Scanner Emulator for VendorTron 2000 server"
-while True:
-  message = None
-  if sock:
+def send():
+  global sock
+  while True:
     message = raw_input()
-    try:
-      sock.send(message)
-      message = None
-    except socket.error:
-      print "[ERROR] not connected to a server"
-      sock = None
-  else:
-    print "Trying to connect to server"
-    while True:
+    if sock:
       try:
-        if not sock: sock = socket.socket()
-        sock.connect((HOST,PORT))
-        print "Successfully connected to server"
-        if message:
-          sock.send(message)
-          message = None
-        break
-      except socket.error:
+        sock.send(message)
+      except:
+        print "[ERROR] not connected to a server"
+        sock = None
+    else:
+      print "[ERROR] not connected to a server"
+      while sock == None:
         time.sleep(4)
+
+def receive():
+  global sock
+  while True:
+    try:
+      sock = socket.socket()
+      sock.connect((HOST,PORT))
+      print "Successfully connected to server"
+    except:
+      time.sleep(4)
+      continue
+    while sock:
+      try:
+        message = sock.recv(500).rstrip()
+        if len(message) == 0:
+          break
+      except:
+        break
+    print "Disconnected from server"
+
+print "RFID scanner emulator for VendorTron 2000 server"
+
+threading.Thread(target=send).start()
+threading.Thread(target=receive).start()
